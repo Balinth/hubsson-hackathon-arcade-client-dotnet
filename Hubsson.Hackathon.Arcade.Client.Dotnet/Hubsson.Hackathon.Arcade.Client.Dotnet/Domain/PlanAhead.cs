aad37d002f9d441ba59c65df82181aaa0825f004
Balinth <balinth.cg@gmail.com>
2023-06-03 16:46:03 +0200

    PlanAhead kind of working

--- Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Domain/PlanAhead.cs
+++ Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Domain/PlanAhead.cs
@@ -7,27 +7,124 @@ public record OtherPlayer(Coords HeadPos, Direction LastDirection);
 public record PlanAheadNode
 {
     public ImmutableHashSet<Coords> Obstacles { get; init; }
-    public PlanAheadNode Parent { get; init; }
+    public PlanAheadNode? Parent { get; init; }
     public ImmutableList<OtherPlayer> OtherPlayers { get; init; }
     public Coords CurrentPos { get; init; }
 
+    public static Direction Strategize(ImmutableHashSet<Coords> startingObstacles, int width, int height, Coords startingPos,
+        ImmutableList<OtherPlayer> startingOtherPlayers, CancellationToken cancellationToken)
+    {
+        PriorityQueue<PlanAheadNode, float> nodes = new PriorityQueue<PlanAheadNode, float>(){};
+
+        var rootNode = new PlanAheadNode()
+        {
+            Obstacles = startingObstacles,
+            Parent = null,
+            OtherPlayers = startingOtherPlayers,
+            CurrentPos = startingPos
+        };
+        
+        nodes.Enqueue(rootNode, 0);
+        
+        while (cancellationToken.IsCancellationRequested is false)
+        {
+            var currentNode = nodes.Dequeue();
+            var newNodes = currentNode.ExpandPlan(width, height);
+            foreach (var newNode in newNodes)
+            {
+                nodes.Enqueue(newNode, newNode.CalcFreedomHeuristicScore(width, height));
+            }
+        }
+
+        var nextStep = nodes.Dequeue().GetFirstMove();
+
+        Console.WriteLine($"Iterations planned ahead: {nextStep.CalcIteration()}");
+
+        var newDir = PathFind.CoordsToDirection(startingPos, nextStep.CurrentPos);
+        if (newDir == Direction.Down)
+        {
+            int breakHere;
+        }
+
+        return newDir;
+    }
+    
     public List<PlanAheadNode> ExpandPlan(
-        string currentPlayerId,
         int mapWidth,
         int mapHeight)
     {
         var possibleDirections = PathFind.GetPossibleDirections(Obstacles, mapWidth, mapHeight, CurrentPos).Select(d => PathFind.CoordsToDirection(CurrentPos, d)).ToList();
         return possibleDirections.Select(d =>
-            this with
+        {
+            var newOtherPlayers = ImmutableList<OtherPlayer>.Empty;
+            var newObstacles = Obstacles;
+            if (Parent is not null)
+                newObstacles = newObstacles.Add(Parent.CurrentPos);
+            newObstacles = newObstacles.Add(PathFind.DirectionToCoords(d, CurrentPos));
+            foreach (var otherPlayer in OtherPlayers)
             {
-                Obstacles = Obstacles,
-                Parent = this
+                var (obstacles, newOtherPlayer) =
+                    ExpandObstaclesWithAssumedPath(newObstacles, mapWidth, mapHeight, otherPlayer);
+                newObstacles = obstacles;
+                newOtherPlayers = newOtherPlayers.Add(newOtherPlayer);
             }
-        ).ToList();
+
+            var newCurrentPos = PathFind.DirectionToCoords(d, CurrentPos);
+            
+            return new PlanAheadNode
+            {
+                OtherPlayers = newOtherPlayers,
+                Obstacles = newObstacles,
+                Parent = this,
+                CurrentPos = newCurrentPos,
+            };
+        }).ToList();
     }
 
-    public ImmutableHashSet<Coords> ExpandObstaclesWithAssumedPaths()
+    public float CalcFreedomHeuristicScore(int width, int height)
     {
-        return null;
+        return (
+            CalcIteration()
+            + PathFind.GetPossibleDirections(Obstacles, width, height, CurrentPos).Count()
+            ) * -1;
+    }
+
+    public int CalcIteration()
+    {
+        var result = 1;
+        var currentParent = Parent;
+        while (currentParent is not null)
+        {
+            result++;
+            currentParent = currentParent.Parent;
+        }
+
+        return result;
+    }
+    public PlanAheadNode GetFirstMove()
+    {
+        var currentNode = this;
+        while (true)
+        {
+            if (currentNode?.Parent?.Parent is null)
+                return currentNode;
+            currentNode = currentNode.Parent;
+        }
+    }
+
+    public static (ImmutableHashSet<Coords>, OtherPlayer) ExpandObstaclesWithAssumedPath(ImmutableHashSet<Coords> obstacles, int width, int height, OtherPlayer otherPlayer)
+    {
+        var possibleDirections = PathFind.GetPossibleDirections(obstacles, width, height, otherPlayer.HeadPos).Select(d => PathFind.CoordsToDirection(otherPlayer.HeadPos, d)).ToList();;
+        
+        var direction =
+            (possibleDirections.Cast<Direction?>().Any(d => d == otherPlayer.LastDirection)
+                ? (Direction?)otherPlayer.LastDirection
+                : possibleDirections.Cast<Direction?>().FirstOrDefault()) ?? Direction.Down;
+        
+        var newPos = PathFind.DirectionToCoords(direction, otherPlayer.HeadPos);
+        return (
+            obstacles.Add(newPos),
+            new OtherPlayer(HeadPos: newPos, LastDirection: direction)
+        );
     }
 }
--- Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
+++ Hubsson.Hackathon.Arcade.Client.Dotnet/Hubsson.Hackathon.Arcade.Client.Dotnet/Services/MatchService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Immutable;
 using System.Diagnostics;
 using Hubsson.Hackathon.Arcade.Client.Dotnet.Domain;
 using Action = Hubsson.Hackathon.Arcade.Client.Dotnet.Domain.Action;
@@ -30,9 +31,45 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
         {
             var stopwatch = new Stopwatch();
             stopwatch.Start();
+            // add new coords to Obstacles..
+            _matchRepository.Obstacles =
+                gameState.players
+                    .Select(p => p.coordinates.LastOrDefault())
+                    .Aggregate(_matchRepository.Obstacles,
+                        (state, coords) => coords is null ? state : state.Add(new Coords(coords.x, coords.y)));
+            
+            var currentPlayerLastPos = gameState.players.Single(p => p.playerId == _matchRepository.CurrentPlayerName).coordinates.Last();
+            var currentPos = new Coords(currentPlayerLastPos.x, currentPlayerLastPos.y);
+            
+            
             // On Each Frame Update return an Action for your player
-            var newDir = PathFind.DumbAvoider(gameState.players, _matchRepository.CurrentPlayerName, gameState.width,
-                gameState.height, _matchRepository.Bias);
+            //var newDir = PathFind.DumbAvoider(gameState.players, _matchRepository.CurrentPlayerName, gameState.width,
+            //    gameState.height, _matchRepository.Bias);
+
+            var cancellationSource = new CancellationTokenSource();
+
+            var timer = new Timer(_ => cancellationSource.Cancel(false), null, TimeSpan.FromMilliseconds(gameState.tickTimeInMs * 0.5),
+                Timeout.InfiniteTimeSpan);
+
+            var otherPlayers =
+                gameState.players
+                    .Where(p => p.playerId != _matchRepository.CurrentPlayerName)
+                    .Select(p =>
+                    {
+                        var lastPos = p.coordinates.Last();
+                        var lastCoords = new Coords(lastPos.x, lastPos.y);
+                        if(p.coordinates.Length < 2)
+                            return new OtherPlayer(lastCoords, Direction.Up);
+                        var lastButOneCoords = p.coordinates[^2];
+                        var lastDirection = PathFind.CoordsToDirection(
+                            new Coords(lastButOneCoords.x, lastButOneCoords.y),
+                            lastCoords
+                        );
+                        return new OtherPlayer(lastCoords, lastDirection);
+                    }).ToImmutableList();
+            var newDir = PlanAheadNode.Strategize(_matchRepository.Obstacles, gameState.width, gameState.height,
+                currentPos, otherPlayers, cancellationSource.Token);
+
 
             _matchRepository.Bias = newDir;
             stopwatch.Stop();
@@ -43,6 +80,7 @@ namespace Hubsson.Hackathon.Arcade.Client.Dotnet.Services
         public class MatchRepository
         {
             public string CurrentPlayerName { get; init; }
+            public ImmutableHashSet<Coords> Obstacles { get; set; } = ImmutableHashSet<Coords>.Empty;
             public Direction Bias { get; set; }
         }
     }
